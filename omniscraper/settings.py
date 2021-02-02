d05a041a23a2b2e89283c87f50053dee9eb0178a
Francis Mutiso <franckmutiso@gmail.com>
2021-02-02 17:31:46 +0300

    added allowed hosts

--- omniscraper/settings.py
+++ omniscraper/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['omniscraper.com']
+ALLOWED_HOSTS = ['omniscraper.herokuapp.com']
 
 
 # Application definition
